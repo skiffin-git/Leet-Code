6d415b1bb32c51de520149f1005bbed6b0f5d31d
Vladislav Semenyutin <semenyutin.v@gmail.com>
2023-03-09 15:32:58 -0800

    finished tic-tac-toe

---
+++ src/Apple_Questions_LeetCode/_1_Tic_Tac_Toe/TicTacToe.java
@@ -0,0 +1,89 @@
+package Apple_Questions_LeetCode._1_Tic_Tac_Toe;
+
+class TicTacToe {
+    char[][] board;
+    int length;
+
+    public TicTacToe(int n) {
+        length = n;
+        board = new char[n][n];
+    }
+
+    public int move(int row, int col, int player) {
+        if (player == 1) {
+            board[row][col] = 'X';
+        } else {
+            board[row][col] = 'O';
+        }
+
+        // check win condition
+        if (checkWin(player, row, col)) {
+            return player;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public boolean checkWin(int player, int row, int col) {
+        char playerChar = (player == 1) ? 'X' : 'O';
+
+        // check row
+        boolean win = true;
+        for(int c = 0; c < length; c++){
+            if(board[row][c] != playerChar){
+                win = false;
+                break;
+            }
+        }
+        if(win){
+            return true;
+        }
+
+        // check col
+        win = true;
+        for(int r = 0; r < length; r++){
+            if(board[r][col] != playerChar){
+                win = false;
+                break;
+            }
+        }
+        if(win) {
+            return true;
+        }
+
+        // check diagonal
+
+        if(row == col){
+            win = true;
+            for(int i = 0; i < length; i++){
+                if(board[i][i] != playerChar){
+                    win = false;
+                    break;
+                }
+            }
+        }
+        if(win) {
+            return true;
+        }
+
+
+        // check anti-diagonal
+
+        if(row + col == length - 1){
+            win = true;
+            for(int i = 0; i < length; i++){
+                if(board[length - i - 1][i] != playerChar){
+                    win = false;
+                    break;
+                }
+            }
+        }
+        return win;
+    }
+}
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
